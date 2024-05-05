5eca931c4edeb87367e6c09701f66466cde9a743
ksinjo <ksinjo@naver.com>
2024-05-05 15:03:57 +0900

    add rock paper scissors game

--- 47reWrite.py
+++ 47reWrite.py
@@ -1,5 +1,5 @@
-# 당신 로드킬 당한 동물을 발견해서 묻어주고 있습니다. 
-# 이 동물은 다이몬드 30캐럿이 위에 있습니다. 
+# 당신 로드킬 당한 동물을 발견해서 묻어주려는 도중에서 
+# 30캐럿 다이아몬드를 꿀꺽한 동물이고 주변에서 이 동물 찾고 있습니다. 
 # 사채를 숨길 25개의 위치 중에서 Z표시를 할 위치를 설정하세요.
 
 #  숨길 리스트 요소 생성 
@@ -18,8 +18,8 @@ letter = pos[0].lower()
 col= ["a","b","c","d","e"]
 
 letter_index = col.index(letter)
-print(pos[0])
-print(pos[1])
+print(f"열의 위치(A,b,c,d,e) : {pos[0]}")
+print(f"행의 위치(0, 1,2,3,4 ) :  {pos[1]}")
 number_index = int(pos[1])-1
 
 map[number_index] [letter_index] = "Z"
---
+++ 48rock-paper-scissors.py
@@ -0,0 +1,52 @@
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+game_images= [rock,paper,scissors]
+import random
+
+user_choice = int(input("0 = rock , 1 = paper 12 = Scissors. What Do U Choose?"))
+print(f"My choice is \n {game_images[user_choice]}")
+
+com_choice = random.randint(0,2)
+print(f"com choice \n {game_images[com_choice]}")
+
+# print(f"{user_choice}\n")
+# print(f"{com_choice}\n")
+
+if user_choice == 0 and com_choice ==2:
+    print("U win")
+elif com_choice == 0 and user_choice == 2:
+    print("U lose")
+
+elif user_choice < com_choice : 
+    print("U lose!")
+
+elif com_choice > user_choice :
+    print("U win")    
+elif user_choice == com_choice :
+    print("it's a draw")
+elif user_choice >=3 or user_choice < 0 :
+    print("you typed an invalid number, you lose!")
