f21c62d19392c5fe45173db22f4b6d9da22318e6
ksinjo <ksinjo@naver.com>
2024-05-05 16:49:41 +0900

    fix bug

--- 48rock-paper-scissors.py
+++ 48rock-paper-scissors.py
@@ -27,26 +27,35 @@ scissors = '''
 game_images= [rock,paper,scissors]
 import random
 
+
+
 user_choice = int(input("0 = rock , 1 = paper 12 = Scissors. What Do U Choose?"))
-print(f"My choice is \n {game_images[user_choice]}")
+if user_choice >= 3 or user_choice < 0:
+    print("U type an invalid number, U lose")
+else:
+    print("This is a valid value.")
+        
+    print(f"My choice is \n {game_images[user_choice]}")
 
-com_choice = random.randint(0,2)
-print(f"com choice \n {game_images[com_choice]}")
+    com_choice = random.randint(0,2)
+    print(f"com choice \n {game_images[com_choice]}")
 
-# print(f"{user_choice}\n")
-# print(f"{com_choice}\n")
+    # print(f"{user_choice}\n")
+    # print(f"{com_choice}\n")
 
-if user_choice == 0 and com_choice ==2:
-    print("U win")
-elif com_choice == 0 and user_choice == 2:
-    print("U lose")
 
-elif user_choice < com_choice : 
-    print("U lose!")
 
-elif com_choice > user_choice :
-    print("U win")    
-elif user_choice == com_choice :
-    print("it's a draw")
-elif user_choice >=3 or user_choice < 0 :
-    print("you typed an invalid number, you lose!")
+    if user_choice == 0 and com_choice ==2:
+        print("U win")
+    elif com_choice == 0 and user_choice == 2:
+        print("U lose")
+
+    elif user_choice < com_choice : 
+        print("U lose!")
+
+    elif com_choice > user_choice :
+        print("U win")    
+    elif user_choice == com_choice :
+        print("it's a draw")
+    elif user_choice >=3 or user_choice < 0 :
+        print("you typed an invalid number, you lose!")
