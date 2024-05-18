a86de9d36b21e85e67761223830aafc3bd652ad1
ksinjo <ksinjo@naver.com>
2024-05-18 23:48:45 +0900

    add docfile

---
+++ CP9/94nestedDic.py
@@ -0,0 +1,14 @@
+#Nesting
+capitals = {
+    "France": "paris",
+    "Germay" : "Berlin"
+}
+
+#Nesting a List in a Dictionary
+
+traval_log = {
+    "France" : ["paris", "Lille","dijon"],
+    "Germany": ["Berlin","Hamburg","stuttgart"],
+
+}
+
