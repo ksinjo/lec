0d33e16c9278314c04de2e60507e30e65ed07d2a
ksinjo <ksinjo@naver.com>
2024-05-26 20:30:37 +0900

    add studyfile

---
+++ CP11/sum.py
@@ -0,0 +1,6 @@
+t = [12,3]
+
+def cal_score(cards):
+    return sum(cards)
+
+print(cal_score(t))
--- Mini/calculator/main.py
+++ Mini/calculator/main.py
@@ -1,57 +1,44 @@
-
-#Calculator 
-
-#Add
+#Calculator
 def add(n1, n2):
-    return n1 + n2
+  return n1 + n2
 
-#Subtract
 def subtract(n1, n2):
-    return n1 - n2
+  return n1 - n2
 
-#Multiply
 def multiply(n1, n2):
-    return n1 * n2
+  return n1 * n2
 
-#Divide
-def Divide(n1,n2):
-    return n1 / n2
+def divide(n1, n2):
+  return n1 / n2
 
 operations = {
-    "+": add,
-    "-": subtract,
-    "*": multiply,
-    "/": Divide
+  "+": add,
+  "-": subtract,
+  "*": multiply,
+  "/": divide
 }
-
-# 사용자에게 피연산자1을 입력받게한다.
+from art import logo
 def calculator():
-    num1 = int(input("what's the firstr num :\n"))
+    print(logo)
+    num1 = float(input("What's the first number?: "))
+    for symbol in operations:
+        print(symbol)
+    should_continue = True # while문 반복 초기값
 
-    # 딕셔너리 연산자들을 보여준다. 
-    for op in operations:
-        print(op)
-    # 입력받은 심볼을 변수에 저장 
-    should_contiune = True
-    
-    while should_contiune:
-        op_symbol = input("Pick an operation")
+    while should_continue: # while문 스코어범위 들여쓰기
+        operation_symbol = input("Pick an operation: ") 
+        num2 = float(input("What's the next number?: "))
+        calculation_function = operations[operation_symbol]
+        answer = calculation_function(num1, num2)
 
-        num2 = int(input("what's the second num :\n"))
-    # 기명성 함수인 calcuation_function 할당 딕셔너리의 키로 접근 
-        calulation_function = operations[op_symbol]
-    # answer 변수는 위의 함수를 실행(인자1,2 받음)
-        answer = calulation_function(num1,num2)
-    # printf(f" num1  opertations num2  answer 4개의 인자를 보여준다.  "")
-        print(f"{num1} {operations} {num2} = {answer}")
-        if input(f"type 'y' to continue calculatin with {answer}: ") == "y":
-            num1 = answer
-        else: 
-            should_contiune=False
-            calculator()
-# op_symbol = input("Pick an operation from the line above:")
-# num3 = int(input("what's the netxt num? :\n"))
-# calulation_function = operations[op_symbol]
-# second_answer = calulation_function(calulation_function(num1,num2),num3)
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
 
-# print(f"{first_answer} {operations} {num3} = {second_answer}")
+        if input(f" type 'y'to continye calculating wiht {answer}, or type 'n' to exit.: ") =="y":
+            num1= answer 
+        else:
+            should_continue = False
+            calculator() 
+       
+calculator() # 재귀적함수 호출 
+#Here we select "*" which overides the "+" we selected on line 26.
+
